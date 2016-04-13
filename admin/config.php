13767ca95dc042d88c13c61be095b8ad019680ff
Pradeep <jpradeep.anna@gmail.com>
2016-04-13 08:36:42 +0530

    Riceday
    Master

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/rice/admin/');
+define('HTTP_CATALOG', 'http://localhost/rice/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/rice/admin/');
+define('HTTPS_CATALOG', 'http://localhost/rice/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\rice/admin/');
+define('DIR_SYSTEM', 'C:\wamp\www\rice/system/');
+define('DIR_LANGUAGE', 'C:\wamp\www\rice/admin/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\rice/admin/view/template/');
+define('DIR_CONFIG', 'C:\wamp\www\rice/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\rice/image/');
+define('DIR_CACHE', 'C:\wamp\www\rice/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\rice/system/download/');
+define('DIR_UPLOAD', 'C:\wamp\www\rice/system/upload/');
+define('DIR_LOGS', 'C:\wamp\www\rice/system/logs/');
+define('DIR_MODIFICATION', 'C:\wamp\www\rice/system/modification/');
+define('DIR_CATALOG', 'C:\wamp\www\rice/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'styleshop');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/rice/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/rice/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\wamp\www\rice/catalog/');
+define('DIR_SYSTEM', 'C:\wamp\www\rice/system/');
+define('DIR_LANGUAGE', 'C:\wamp\www\rice/catalog/language/');
+define('DIR_TEMPLATE', 'C:\wamp\www\rice/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\wamp\www\rice/system/config/');
+define('DIR_IMAGE', 'C:\wamp\www\rice/image/');
+define('DIR_CACHE', 'C:\wamp\www\rice/system/cache/');
+define('DIR_DOWNLOAD', 'C:\wamp\www\rice/system/download/');
+define('DIR_UPLOAD', 'C:\wamp\www\rice/system/upload/');
+define('DIR_MODIFICATION', 'C:\wamp\www\rice/system/modification/');
+define('DIR_LOGS', 'C:\wamp\www\rice/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'styleshop');
+define('DB_PREFIX', 'oc_');
